b5cf06a0ad8b801e2026752a635929b7b72b51da
penglei <penglei@ybyte.org>
2022-11-30 14:34:22 +0800

    update rime-config impl

--- hm-modules/darwin/rime-config.nix
+++ hm-modules/darwin/rime-config.nix
@@ -1,27 +1,41 @@
-{
-  home.file."Library/Rime/lua".source = ../../files/rime/lua;
-  home.file."Library/Rime/opencc".source = ../../files/rime/opencc;
-  home.file."Library/Rime/custom_phrase.txt".source = ../../files/rime/custom_phrase.txt;
-  home.file."Library/Rime/default.custom.yaml".source = ../../files/rime/default.custom.yaml;
-  home.file."Library/Rime/double_pinyin_flypy.custom.yaml".source = ../../files/rime/double_pinyin_flypy.custom.yaml;
-  home.file."Library/Rime/double_pinyin_flypy.schema.yaml".source = ../../files/rime/double_pinyin_flypy.schema.yaml;
-  home.file."Library/Rime/easy_en.dict.yaml".source = ../../files/rime/easy_en.dict.yaml;
-  home.file."Library/Rime/easy_en.schema.yaml".source = ../../files/rime/easy_en.schema.yaml;
-  home.file."Library/Rime/grammar.yaml".source = ../../files/rime/grammar.yaml;
-  home.file."Library/Rime/installation.yaml".source = ../../files/rime/installation.yaml;
-  home.file."Library/Rime/luna_pinyin.chengyusuyu.dict.yaml".source = ../../files/rime/luna_pinyin.chengyusuyu.dict.yaml;
-  home.file."Library/Rime/luna_pinyin.jisuanjicihuidaquan.dict.yaml".source = ../../files/rime/luna_pinyin.jisuanjicihuidaquan.dict.yaml;
-  home.file."Library/Rime/luna_pinyin.kaifadashenzhuanyongciku.dict.yaml".source = ../../files/rime/luna_pinyin.kaifadashenzhuanyongciku.dict.yaml;
-  home.file."Library/Rime/luna_pinyin.kaomoji.dict.yaml".source = ../../files/rime/luna_pinyin.kaomoji.dict.yaml;
-  home.file."Library/Rime/luna_pinyin.my-symbols.dict.yaml".source = ../../files/rime/luna_pinyin.my-symbols.dict.yaml;
-  home.file."Library/Rime/luna_pinyin.sijixingzhenquhuadimingciku.dict.yaml".source = ../../files/rime/luna_pinyin.sijixingzhenquhuadimingciku.dict.yaml;
-  home.file."Library/Rime/luna_pinyin.wangluoliuxingxinci.dict.yaml".source = ../../files/rime/luna_pinyin.wangluoliuxingxinci.dict.yaml;
-  home.file."Library/Rime/luna_pinyin.zhongguolishicihuidaquan.dict.yaml".source = ../../files/rime/luna_pinyin.zhongguolishicihuidaquan.dict.yaml;
-  home.file."Library/Rime/numbers.schema.yaml".source = ../../files/rime/numbers.schema.yaml;
-  home.file."Library/Rime/pinyin_dicts.dict.yaml".source = ../../files/rime/pinyin_dicts.dict.yaml;
-  home.file."Library/Rime/pinyin_simp.schema.yaml".source = ../../files/rime/pinyin_simp.schema.yaml;
-  home.file."Library/Rime/pinyin_simp_mini.dict.yaml".source = ../../files/rime/pinyin_simp_mini.dict.yaml;
-  home.file."Library/Rime/rime.lua".source = ../../files/rime/rime.lua;
-  home.file."Library/Rime/squirrel.custom.yaml".source = ../../files/rime/squirrel.custom.yaml;
+{pkgs, config, ...}:
 
-}
+let
+  lib = pkgs.lib; 
+
+  pathToFilename = p: "Library/Rime/" + lib.lists.last (lib.strings.split "/" (builtins.toString p));
+
+  rimeFiles = [
+    ../../files/rime/lua
+    ../../files/rime/opencc
+    ../../files/rime/custom_phrase.txt
+    ../../files/rime/default.custom.yaml
+    ../../files/rime/double_pinyin_flypy.custom.yaml
+    ../../files/rime/double_pinyin_flypy.schema.yaml
+    ../../files/rime/easy_en.dict.yaml
+    ../../files/rime/easy_en.schema.yaml
+    ../../files/rime/grammar.yaml
+    ../../files/rime/installation.yaml
+    ../../files/rime/luna_pinyin.chengyusuyu.dict.yaml
+    ../../files/rime/luna_pinyin.jisuanjicihuidaquan.dict.yaml
+    ../../files/rime/luna_pinyin.kaifadashenzhuanyongciku.dict.yaml
+    ../../files/rime/luna_pinyin.kaomoji.dict.yaml
+    ../../files/rime/luna_pinyin.my-symbols.dict.yaml
+    ../../files/rime/luna_pinyin.sijixingzhenquhuadimingciku.dict.yaml
+    ../../files/rime/luna_pinyin.wangluoliuxingxinci.dict.yaml
+    ../../files/rime/luna_pinyin.zhongguolishicihuidaquan.dict.yaml
+    ../../files/rime/numbers.schema.yaml
+    ../../files/rime/pinyin_dicts.dict.yaml
+    ../../files/rime/pinyin_simp.schema.yaml
+    ../../files/rime/pinyin_simp_mini.dict.yaml
+    ../../files/rime/rime.lua
+    ../../files/rime/squirrel.custom.yaml
+  ];
+
+in
+  {
+    home.file =
+      builtins.listToAttrs (map (p: { name = pathToFilename p; value = {source = p;}; }) rimeFiles);
+      #lib.lists.foldr (p: cfg: cfg // { ${pathToFilename p} = { source = p;};}) {} rimeFiles;
+  }
+
