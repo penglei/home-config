b61a4bcbde0a02f63df2970a7f68e2fd9a35bdc6
penglei <penglei@ybyte.org>
2022-12-01 21:07:52 +0800

    up

--- pkgs/nix-cleaner.nix
+++ pkgs/nix-cleaner.nix
@@ -3,17 +3,21 @@
 writeShellApplication {
   name = "nix-cleaner";
   text = ''
-    if type home-manager > /dev/null 2>&1
-    then
+    if type home-manager > /dev/null 2>&1; then
       home-manager expire-generations "$(home-manager generations|head -n 1 | awk -F ' : ' '{print $1}')"
     fi
 
     nix profile wipe-history
     #nix-env --delete-generations old
 
-    if [[ -f /nix/var/nix/profiles/system ]]; then
+    if [[ -h /nix/var/nix/profiles/per-user/''${USER}/home-manager ]]; then
+      nix profile wipe-history --profile "/nix/var/nix/profiles/per-user/''${USER}/home-manager"
+    fi
+
+    if [[ -h /nix/var/nix/profiles/system ]]; then
       sudo nix profile wipe-history --profile /nix/var/nix/profiles/system
       #sudo nix-env -p /nix/var/nix/profiles/system --delete-generations old
     fi
+
   '';
 }
