e62305e426828468c520ad2652e0d603dbf5e538
penglei <penglei@ybyte.org>
2022-12-02 15:01:46 +0800

    update darwin settings

--- hm-modules/darwin/settings.nix
+++ hm-modules/darwin/settings.nix
@@ -5,6 +5,7 @@
   #/usr/libexec/PlistBuddy -c "Set 'Window Settings':'Man Page':rowCount 50" "$plistfile"
   #(/usr/libexec/PlistBuddy -c "Delete 'Window Settings':'Man Page':columnCount" "$plistfile" || true)
   #/usr/libexec/PlistBuddy -c "Add 'Window Settings':'Man Page':columnCount integer 190" "$plistfile"
+  defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
   '';
 }
 
