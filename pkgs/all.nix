bd15cca656a84701769de7161941b4f61e0126ff
penglei <penglei@ybyte.org>
2022-12-13 10:58:09 +0800

    more git config

--- pkgs/all.nix
+++ pkgs/all.nix
@@ -4,14 +4,13 @@ let
 in
 with pkgs;
 {
-  mynixcleaner = callPackage ./nix-cleaner.nix {};
+  createScript = callPackage ./createscript.nix {};
 
   nerdfonts = (prev.nerdfonts.override { fonts = [ "FiraCode" "DejaVuSansMono" "DroidSansMono" "Monoid"]; });
 
   kubectl-kubectx = callPackage ./kubectl/kubectx.nix {};
   kubectl-kubecm = callPackage ./kubectl/kubecm.nix {};
   kubectl-nodeshell = callPackage ./kubectl/nodeshell.nix {};
-
 
   utm = (prev.utm.overrideAttrs (finalAttrs: previousAttrs: rec {
     version = "4.1.0";
@@ -29,6 +28,8 @@ with pkgs;
   preview_open = callPackage ./darwin/preview.nix {};
 
   isabelle_app = callPackage ./darwin/isabelle.nix {};
+
+  mynixcleaner = callPackage ./nix-cleaner.nix {};
 
   nix-direnv = callPackage ./nix-direnv {};
 }
