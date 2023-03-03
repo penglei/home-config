afdd1c26462cfcacc737ecfc3b7954cc50e7004c
penglei <penglei@ybyte.org>
2023-03-03 11:05:57 +0800

    TODO local postgresql

---
+++ hm-modules/darwin/postgresql.nix
@@ -0,0 +1,25 @@
+{ pkgs
+, config
+, ...
+}:
+
+let
+  homeProfilePath = path: "${config.home.homeDirectory}/${path}";
+in {
+  launchd.agents.postgresql = {
+    enable = true;
+
+    config = {
+      ProgramArguments = [ "${pkgs.postgresql}/bin/postgres" ];
+      EnvironmentVariables = {
+        "PATH" = "${homeProfilePath ".nix-profile/bin"}:${homeProfilePath ".local/bin"}:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:";
+        "SHELL" = "/bin/sh";
+      };
+      KeepAlive = true;
+      RunAtLoad = false;
+      StandardErrorPath = "/tmp/postgresql.err.log";
+      StandardOutPath = "/tmp/postgresql.out.log";
+    };
+  };
+}
+
