4ec975c67b9122bb511f98023e01124b1f586527
penglei <penglei@ybyte.org>
2023-04-21 00:26:34 +0800

    upgrade yabai

--- pkgs/all.nix
+++ pkgs/all.nix
@@ -50,4 +50,7 @@ with pkgs;
   adobe-reader = callPackage ./darwin/adobe-reader.nix {};
   keycastr = callPackage ./darwin/keycastr.nix {};
   alttab = callPackage ./darwin/alttab.nix {};
+  yabai = darwin.apple_sdk_11_0.callPackage ./darwin/yabai.nix {
+    inherit (darwin.apple_sdk_11_0.frameworks) SkyLight Cocoa Carbon ScriptingBridge;
+  };
 }
