76b005a6914f101fa2e2733ba85734b6df8fd4ef
penglei <penglei@ybyte.org>
2023-08-15 14:56:36 +0800

    feat: upgrade zsh hm module

--- hm-modules/zsh.nix
+++ hm-modules/zsh.nix
@@ -20,7 +20,7 @@ in
     enable = true;
     enableCompletion = true;
     enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
     autocd = true;
     defaultKeymap = "emacs";
     history = {
